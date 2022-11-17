ec69093f9b3262a6402a88e757529093e3fade02
Thomas Cilloni <tcilloni@outlook.com>
2022-11-17 15:07:44 -0600

    default evaluation parameters for fiftyone

--- const.py
+++ const.py
@@ -1,3 +1,20 @@
 import torch
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+FIFTYONE_EVAL_PARAMS = {
+    'coco': {
+        'pred_field':   'predictions', 
+        'gt_field':     'detections', 
+        'eval_key':     'eval',
+        'method':       'coco',
+        'compute_mAP':  True
+    },
+    'pascal': {
+        'pred_field':   'predictions', 
+        'gt_field':     'ground_truth', 
+        'eval_key':     'eval',
+        'method':       'open-images',
+        'compute_mAP':  True
+    }
+}
